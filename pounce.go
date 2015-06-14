fd3a8566684445afaccf39a9b39798d9df551a26
Brian Tomlinson <darthlukan@gmail.com>
2015-06-14 18:24:26 +0200

    Start rewrite, use cli package for convenience, think about properly using channels for concurrency

--- pounce.go
+++ pounce.go
@@ -29,199 +29,125 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
+	"github.com/codegangsta/cli"
 	"github.com/guelfey/go.dbus"
 	"io"
 	"net/http"
 	"os"
-	"strings"
-	"time"
 )
 
-// download takes a url of type string as an argument
-// and returns a response pointer.
-func download(url string) *http.Response {
-
-	fmt.Printf("Downloading...\n")
-	resp, err := http.Get(url)
-
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Download complete!\n")
-
-	return resp
-}
-
-// create takes a filename of type string as an argument
-// and returns a file pointer.  It basically calls
-// os.Create(filename) and checks err.
-func create(filename string) *os.File {
-
-	file, err := os.Create(filename)
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Created %v ...\n", filename)
-
-	return file
-}
-
-func multiDownload(url, destination string, channel chan<- bool) {
-
-	fmt.Printf("Downloading...\n")
-	resp, err := http.Get(url)
-	createDone := make(chan bool)
-	go multiCreate(destination, resp, createDone)
-
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Download complete!\n")
-	channel <- true
-	<-createDone
-}
-
-func multiCreate(destination string, response *http.Response, createDone chan<- bool) {
-
-	fmt.Printf("Creating %v ...\n", destination)
-	file, err := os.Create(destination)
-	defer file.Close()
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Writing file %v...\n", destination)
-	complete, err := io.Copy(file, response.Body)
-	defer response.Body.Close()
-	fmt.Printf("Wrote %v bytes to file %v\n", complete, destination)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	createDone <- true
-}
-
-func readFile(infilename, destination string) {
-
-	file, err := os.Open(infilename)
-
-	if err != nil {
-		panic(err)
-	}
-
-	scanner := bufio.NewScanner(file)
-
-	lineCount := 0
-	channel := make(chan bool)
-	for scanner.Scan() {
-		lineCount++
-
-		line := scanner.Text()
-		splitCount := strings.Count(line, "/")
-		endOfSplit := strings.SplitAfterN(line, "/", splitCount)
-		filename := string(endOfSplit[len(endOfSplit)-1])
-
-		if strings.Count(filename, "/") > 0 {
-			filename = string(strings.SplitAfterN(filename, "/", 2)[1])
-		}
-
-		toSave := fmt.Sprintf("%v%v", destination, filename)
-		go multiDownload(line, toSave, channel)
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Println(err)
-	}
-
-	for i := 0; i < lineCount; i++ {
-		<-channel
-	}
-}
-
-// save takes a file of type io.Writer and a response pointer
-// as an argument so that it can return a size as type int64.
-// It calls io.Copy, handles any errors, and returns.
-func save(file io.Writer, resp *http.Response) int64 {
-
-	fmt.Printf("Writing file...\n")
-	complete, err := io.Copy(file, resp.Body)
-
-	if err != nil {
-		panic(err)
-	}
-
-	return complete
-}
-
-// notify takes a message string and channel as an
-// argument and displays a dbus notification.
-func notify(msg string, done chan<- bool) {
-
+func notify(msg string) {
 	conn, err := dbus.SessionBus()
-
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Unable to connect to dbus session bus: %v\n", err)
 	}
 
 	obj := conn.Object("org.freedesktop.Notifications", "/org/freedesktop/Notifications")
 	call := obj.Call("org.freedesktop.Notifications.Notify", 0, "", uint32(0),
-		"", "GoPounce", msg, []string{}, map[string]dbus.Variant{}, int32(5000))
-
+		"", "pounce", msg, []string{}, map[string]dbus.Variant{}, int32(5000))
 	if call.Err != nil {
-		fmt.Println(call.Err)
+		fmt.Printf("Error while trying to send notification: %v\n", call.Err)
 	}
-	done <- true
+}
+
+func download(url string, response chan<- *http.Response) {
+	fmt.Printf("Retreiving from %v\n", url)
+	resp, err := http.Get(url)
+
+	if err != nil {
+		fmt.Printf("Unable to get resource from '%v'. Error: %v\n", url, err)
+	}
+	response <- resp
+}
+
+func multiDownload(urls []string, destination string) {
+	resp := make(chan *http.Response)
+	for _, url := range urls {
+		go download(url, resp)
+	}
+	for {
+		select {
+		case r := <-resp:
+			defer r.Body.Close()
+		}
+	}
+}
+
+func readFile(filename string) []string {
+	var urls []string
+
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("Problem opening input file!")
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		urls = append(urls, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Problem reading input file!")
+		panic(err)
+	}
+
+	return urls
+}
+
+func save(file io.Writer, resp *http.Response) int64 {
+	complete, err := io.Copy(file, resp.Body)
+	if err != nil {
+		fmt.Printf("Unable to write file, error: %v\n", err)
+	}
+	return complete
 }
 
 func main() {
-
-	var input string
-	var output string
-	var msg string
-
-	done := make(chan bool)
-
-	flag.StringVar(&input, "url", "", "URL to get")
-	flag.StringVar(&input, "infile", "", "An input file with URLs on each line that are newline delimitted.")
-	flag.StringVar(&output, "filename", "", "Destination file to write.")
-	flag.StringVar(&output, "directory", "", "Destination directory to write into with trailing '/' (only works with -infile flag)")
-	flag.Parse()
-	flag.Args()
-
-	if input == "" {
-		input = flag.Arg(0)
+	app := cli.NewApp()
+	app.Name = "pounce"
+	app.Version = "0.0.2"
+	app.Usage = "A very simple file downloader in the vein of wget."
+	app.Authors = []cli.Author{cli.Author{
+		Name:  "Brian Tomlinson",
+		Email: "darthlukan@gmail.com",
+	}}
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "url, u",
+			Value: "",
+			Usage: "The input URL",
+		},
+		cli.StringFlag{
+			Name:  "file, f",
+			Value: "",
+			Usage: "The input file",
+		},
+		cli.StringFlag{
+			Name:  "outfile, o",
+			Value: "",
+			Usage: "The output file",
+		},
+		cli.StringFlag{
+			Name:  "dir, d",
+			Value: "",
+			Usage: "The output directory",
+		},
 	}
+	app.Action = func(c *cli.Context) {
+		url := c.String("url")
+		inFile := c.String("file")
+		outFile := c.String("outfile")
+		outDir := c.String("dir")
 
-	if output == "" {
-		output = flag.Arg(1)
+		if url == "" && inFile == "" {
+			url = c.Args()[0]
+		}
+
+		if outFile == "" && outDir == "" {
+			outFile = fmt.Sprintf("%v/pounced.txt", os.Getenv("HOME"))
+		}
 	}
-
-	start := time.Now()
-
-	// TODO: Refactor, this doesn't conform to DRY.
-	if strings.Contains(input, "http") || strings.Contains(input, "www") {
-		resp := download(input)
-		defer resp.Body.Close()
-		file := create(output)
-		defer file.Close()
-		complete := save(file, resp)
-
-		end := time.Now()
-		size := complete / 1024
-		msg = fmt.Sprintf("Downloaded ~%v kb in %v \n", size, end.Sub(start))
-	} else {
-		readFile(input, output)
-		end := time.Now()
-		msg = fmt.Sprintf("Downloaded contents of %v in %v\n", input, end.Sub(start))
-	}
-
-	go notify(msg, done)
-	fmt.Printf(msg)
-	<-done
+	app.Run(os.Args)
 }
